efa0794d5cd853fa57fb64d7484708c0ec0e2c08
Matt Newbigging <mrnewbigging@gmail.com>
2024-03-29 17:20:42 +0000

    basic movement

--- src/game/player.ts
+++ src/game/player.ts
@@ -5,12 +5,12 @@ import { easeInCubic, easeInOutSine, easeInSine } from "../utils/math";
 
 export class Player {
   direction = new THREE.Vector3();
-  acceleration = new THREE.Vector3();
-  accelerationRampTime = new THREE.Vector3();
-  accelerationRampDir = new THREE.Vector3();
   velocity = new THREE.Vector3();
-  effectiveSpeed = new THREE.Vector3();
-  speed = 20;
+  speed = 50;
+  acceleration = 0;
+  accRampTime = 0;
+  accRampSpeed = 1.5;
+  accRampDir = 0;
 
   constructor(
     public ship: THREE.Object3D,
@@ -20,120 +20,53 @@ export class Player {
   setup() {
     // Face the right way
     this.ship.rotateY(Math.PI);
-
-    // Add input listeners
-    this.keyboardListener.on("a", this.onPressLeft);
-    this.keyboardListener.onRelease("a", this.onReleaseLeft);
-    this.keyboardListener.on("d", this.onPressRight);
-    this.keyboardListener.onRelease("d", this.onReleaseRight);
-    this.keyboardListener.on("w", this.onPressForward);
-    this.keyboardListener.onRelease("w", this.onReleaseForward);
-    this.keyboardListener.on("s", this.onPressBackward);
-    this.keyboardListener.onRelease("s", this.onReleaseBackward);
   }
 
   update(dt: number) {
-    // Ramp timer ticks along according to ramp direction - dt is tick rate for now
-    this.accelerationRampTime.add(
-      this.accelerationRampDir.clone().multiplyScalar(dt)
-    );
+    // Get movement direction from input for this frame
+    const forwardPressed = this.keyboardListener.isKeyPressed("w");
+    const backwardPressed = this.keyboardListener.isKeyPressed("s");
+    const leftPressed = this.keyboardListener.isKeyPressed("a");
+    const rightPressed = this.keyboardListener.isKeyPressed("d");
 
-    // Clamp it between 0 and 1
-    this.accelerationRampTime.clampScalar(0, 1);
+    if (forwardPressed) {
+      this.direction.z = -1;
+    }
+    if (backwardPressed) {
+      this.direction.z = 1;
+    }
+    if (leftPressed) {
+      this.direction.x = -1;
+    }
+    if (rightPressed) {
+      this.direction.x = 1;
+    }
 
-    // Feed into easing function to get acceleration
-    this.acceleration.set(
-      easeInOutSine(this.accelerationRampTime.x),
-      easeInOutSine(this.accelerationRampTime.y),
-      easeInOutSine(this.accelerationRampTime.z)
-    );
+    // Normalise direction so diagonals aren't faster
+    this.direction.normalize();
 
-    // Effective takes into account acceleration modifier and delta time
-    this.effectiveSpeed.copy(
-      this.acceleration.clone().multiplyScalar(this.speed * dt)
-    );
+    // If any inputs are pressed, accelerate
+    if (forwardPressed || backwardPressed || leftPressed || rightPressed) {
+      this.accRampDir = 1;
+    } else {
+      // If no inputs are pressed, decelerate
+      this.accRampDir = -1;
+    }
 
-    // Velocity is direction * effective speed
-    this.velocity = this.direction.clone().multiply(this.effectiveSpeed);
+    // Tick the acceleration ramp timer, normalise it to 0..1
+    this.accRampTime += this.accRampDir * dt * this.accRampSpeed;
+    this.accRampTime = clamp(this.accRampTime, 0, 1);
 
-    // Add to current position
+    // Work out acceleration with new time value
+    this.acceleration = easeInSine(this.accRampTime);
+
+    // Effective speed takes acceleration and delta time into account
+    const effectiveSpeed = this.speed * this.acceleration * dt;
+
+    // Velocity describes the next point in space for the ship
+    this.velocity.copy(this.direction.multiplyScalar(effectiveSpeed));
+
+    // Add velocity to current position to move the ship by that much
     this.ship.position.add(this.velocity);
   }
-
-  private onPressForward = () => {
-    // Now moving forwards
-    this.direction.z = -1;
-    // Ensure acceleration ramp direction is moving fowards
-    this.accelerationRampDir.z = 1;
-  };
-
-  private onReleaseForward = () => {
-    // If moving forwards
-    if (this.direction.z === -1) {
-      // Can decelerate
-      this.accelerationRampDir.z = -1;
-    }
-  };
-
-  private onPressBackward = () => {
-    // Now moving backwards
-    this.direction.z = 1;
-    // Ensure acceleration ramp direction is moving fowards
-    this.accelerationRampDir.z = 1;
-  };
-
-  private onReleaseBackward = () => {
-    // If moving backwards
-    if (this.direction.z === 1) {
-      // Can decelerate
-      this.accelerationRampDir.z = -1;
-    }
-  };
-
-  private onPressLeft = () => {
-    // Now moving left
-    this.direction.x = -1;
-    // Ensure acceleration ramp direction is moving fowards
-    this.accelerationRampDir.x = 1;
-  };
-
-  private onReleaseLeft = () => {
-    // If moving left
-    if (this.direction.x === -1) {
-      // Can decelerate
-      this.accelerationRampDir.x = -1;
-    }
-  };
-
-  private onPressRight = () => {
-    // Now moving right
-    this.direction.x = 1;
-    // Ensure acceleration ramp direction is moving fowards
-    this.accelerationRampDir.x = 1;
-  };
-
-  private onReleaseRight = () => {
-    // If moving right
-    if (this.direction.x === 1) {
-      this.accelerationRampDir.x = -1;
-    }
-  };
-
-  /**
-   * On pressing a direction button:
-   *
-   * - Ramp up direction should be positive
-   * - If moving in opposite direction, overrides previous opposite direction
-   * - E.g if holding & moving right, then press left - should stop moving right and move left
-   *
-   * On moving in opposite direction / moving in direction not currently moving in
-   *
-   * - Ramp up time should be lowered to achieve a sort of speed up effect
-   * - Ideally done based on axis, not across everything
-   * - E.g if moving left and pressing forward, forward momentum has ramp up but left has already ramped up
-   *
-   * On releasing all buttons for a particular input direction:
-   *
-   * - Ramp up direction should be negative for the given input direction value
-   */
 }
